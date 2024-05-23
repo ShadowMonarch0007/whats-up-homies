6ccc5076755d49d6a5091bb882b89773bb434db5
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-24 01:29:59 +0530

    23.05.2024

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,12 +4,14 @@ import pfp from "@/components/footer/img/pfp.jpg";
 import FollowMe from "./subcomponents/followme";
 import EmailSnppet from "./subcomponents/emailSnippet";
 import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { WavyBackground } from "../ui/wavy-background";
 import { Vortex } from "../ui/vortex";
 
 export default async function Footer() {
     return (
         <div className="overflow-hidden p-10 lg:p-0 bg-black">
-            <Vortex>
+            {/* <Vortex> */}
+            <WavyBackground>
                 <div className="flex flex-col lg:flex-row items-center justify-evenly p-5 lg:p-16">
                     {/* <div className="logo flex items-center justify-center">
                         <BackgroundGradient><Image className="rounded-3xl p-0.5" src={pfp} height={200} width={200} /></BackgroundGradient>
@@ -25,7 +27,8 @@ export default async function Footer() {
                     </div>
                 </div>
                 <div className="text-[#e0e0e0] flex text-center align-middle justify-center py-10">Copyright © 2024 - All rights reserved</div>
-            </Vortex>
+            </WavyBackground>
+            {/* </Vortex> */}
         </div>
     );
 }
--- src/components/footer/subcomponents/emailSnippet.jsx
+++ src/components/footer/subcomponents/emailSnippet.jsx
@@ -7,7 +7,7 @@ export default async function EmailSnppet() {
         <div className="p-5">
             <h1 className="text-white font-semibold text-4xl font-sans mb-5">Mail</h1>
             <div className="flex justify-evenly gap-2 group">
-                <div className="mail-icon backdrop:blur-lg group-hover:bg-[#e0e0e0] bg-[#e0e0e0] md:bg-[#333333]/80 lg:bg-[#333333]/80 rounded-xl h-11 w-11 py-2 px-[5px]">
+                <div className="mail-icon backdrop:blur-lg group-hover:bg-[#e0e0e0] bg-[#e0e0e0] md:bg-[#333333]/70 lg:bg-[#333333]/70 rounded-xl h-11 w-11 py-2 px-[5px]">
                     <a href="https://mail.google.com/mail/u/4/#inbox?compose=CllgCJfnbWjFqccwvCtQDQkztVNGDdTpkldNgpnZXGNdZtKJkPWMDvQQTCLQVrSbSSllfrLbqcL"><svg xmlns="http://www.w3.org/2000/svg" viewBox="52 42 88 66">
                         <path fill="#4285f4" d="M58 108h14V74L52 59v43c0 3.32 2.69 6 6 6" />
                         <path fill="#34a853" d="M120 108h14c3.32 0 6-2.69 6-6V59l-20 15" />
@@ -16,7 +16,7 @@ export default async function EmailSnppet() {
                         <path fill="#c5221f" d="M52 51v8l20 15V48l-5.6-4.2c-5.94-4.45-14.4-.22-14.4 7.2" />
                     </svg></a>                    
                 </div>
-                <Snippet symbol="" className="emailsnip hover:bg-[#333333]/80 bg-[#333333]/80 text-white text-sm">aditya.anil.chandra@gmail.com</Snippet>
+                <Snippet symbol="" className="emailsnip hover:bg-[#333333]/70 bg-[#333333]/70 text-white text-sm">aditya.anil.chandra@gmail.com</Snippet>
             </div>
         </div>
     );
