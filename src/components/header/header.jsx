07f2fb35fc6500c97a4b8972333972c03162b9e4
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-25 02:09:38 +0530

    24.05.2024

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Image from "next/image";
 import ThemeButton from "./subcomponents/themeButton";
+import logo from '@/components/header/svg/adityalogo.svg'
 
 export default async function Headers() {
     return(
         <div className="flex items-center justify-between py-5 px-5 lg:px-10">
             <div>
-
+                <Image className="dark:bg-white rounded-lg" src={logo} height={48} width={48}/>
             </div>
             <div>
                 <ThemeButton/>
