f1a1037d91691556b22546353612ab6a9b620795
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-26 19:20:52 +0530

    26.05.2024

--- src/components/hambar/hambar.jsx
+++ src/components/hambar/hambar.jsx
@@ -3,8 +3,8 @@ import HamSheet from "./subcomponents/hamSheet";
 
 export default async function Hambar({text}) {
     return (
-        <div className="px-5 lg:px-10 flex items-center justify-between">
-            <div className="absolute top-5 left-5">
+        <div className="fixed z-[52] top-0 px-5 lg:px-10 flex items-center justify-between">
+            <div className="absolute top-2.5 left-5">
                 <HamSheet/>
             </div>
         </div>
--- src/components/hambar/subcomponents/hamSheet.jsx
+++ src/components/hambar/subcomponents/hamSheet.jsx
@@ -15,7 +15,7 @@ import Link from "next/link";
 
 export default async function HamSheet() {
     return (
-        <div>
+        <div className="absolute z-[100]">
             <Sheet>
                 <SheetTrigger>
                     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"></line><line x1="4" x2="20" y1="6" y2="6"></line><line x1="4" x2="20" y1="18" y2="18"></line></svg>
