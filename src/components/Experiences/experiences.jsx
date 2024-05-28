4f422f44002fb5f0045b4ae6667981a104652b81
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-29 01:37:49 +0530

    29.05.2024

--- src/components/Experiences/experiences.jsx
+++ src/components/Experiences/experiences.jsx
@@ -5,7 +5,7 @@ import MovingButtonDemo from "./subcomponents/movingButtonDemo";
 
 export default async function MyExperiences() {
     return (
-        <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-5">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-5">
             <div className="col-span-1 row-span-1"><MovingButtonDemo img={mobi32} imgClass={"dark:bg-white h-24 w-24 lg:h-36 lg:w-36"} title={"Front-end SDE Intern"} note={"Assisted remotely in the development of web-based platforms using Next.js & Tailwind CSS enhancing interactivity."} /></div>
             <div className="col-span-1 row-span-1"><MovingButtonDemo img={Astro} imgClass={"bg-black dark:bg-transparent p-2 h-24 w-24 lg:h-36 lg:w-36"} title={"Website Developer"} note={"Design and Developed website using React.js for my college club AstroNITR."} /></div>
         </div>
