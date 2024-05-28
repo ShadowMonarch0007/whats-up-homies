326cbf03ffac8afc4f6df2266ac9a1736ae814dd
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-29 00:57:03 +0530

    29.05.2024

--- src/components/Experiences/experiences.jsx
+++ src/components/Experiences/experiences.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import mobi32 from "@/components/Experiences/img/32mobi.webp";
+import Astro from "@/components/Experiences/img/Astronitr.png";
 import MovingButtonDemo from "./subcomponents/movingButtonDemo";
 
 export default async function MyExperiences() {
     return (
         <div className="w-full grid grid-cols-1 lg:grid-cols-2 gap-5">
-            <div className="col-span-1 row-span-1"><MovingButtonDemo img={mobi32} title={"Front-end SDE Intern"} note={"Assisted remotely in the development of web-based platforms using Next.js & Tailwind CSS enhancing interactivity."} /></div>
-            <div className="col-span-1 row-span-1"><MovingButtonDemo img={mobi32} title={"Front-end SDE Intern"} note={"Assisted remotely in the development of web-based platforms using Next.js & Tailwind CSS enhancing interactivity."} /></div>
+            <div className="col-span-1 row-span-1"><MovingButtonDemo img={mobi32} imgClass={"dark:bg-white h-24 w-24 lg:h-36 lg:w-36"} title={"Front-end SDE Intern"} note={"Assisted remotely in the development of web-based platforms using Next.js & Tailwind CSS enhancing interactivity."} /></div>
+            <div className="col-span-1 row-span-1"><MovingButtonDemo img={Astro} imgClass={"bg-black dark:bg-transparent p-2 h-24 w-24 lg:h-36 lg:w-36"} title={"Website Developer"} note={"Design and Developed website using React.js for my college club AstroNITR."} /></div>
         </div>
     );
 }
--- src/components/Experiences/subcomponents/movingButtonDemo.jsx
+++ src/components/Experiences/subcomponents/movingButtonDemo.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { MovingButton } from "@/components/ui/wobble-card";
 
-export default async function MovingButtonDemo({img, title, note}){
+export default async function MovingButtonDemo({img, imgClass, title, note}){
     return(
         <div>
             <MovingButton
@@ -10,11 +10,11 @@ export default async function MovingButtonDemo({img, title, note}){
                     borderRadius="1.75rem"
                     className="w-full flex-1 items-center justify-center"
                 >
-                    <div className="p-10 w-full flex gap-5 items-center justify-center">
-                        <div className="w-full lg:w-1/2 flex dark:bg-white rounded-xl">
-                            <Image src={img}/>
+                    <div className="p-5 lg:p-10 flex items-center justify-between">
+                        <div className="flex items-center justify-center">
+                            <Image className={`${imgClass} rounded-xl`} src={img}/>
                         </div>
-                        <div className="lg:ms-5 flex flex-col gap-2.5">
+                        <div className="w-2/3 lg:ms-5 flex flex-col gap-2.5">
                             <h1 className="text-start text-lg lg:text-2xl font-light lg:font-medium text-[#202020] dark:text-white">{title}</h1>
                             <p className="text-start text-[10px] leading-4 lg:leading:5 lg:text-base font-extralight lg:font-light text-[#606060] dark:text-[#e0e0e0]">{note}</p>
                         </div>
