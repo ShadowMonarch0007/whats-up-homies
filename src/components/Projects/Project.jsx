35d89d8a2e1c8566f71dbbb1d4640245875ea527
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-05-30 01:49:06 +0530

    29.05.2024

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,42 +1,42 @@
 import React from "react";
 import { ThreeDCardDemo } from "./subcomponents/card3d";
 import Image from "next/image";
-import OldPortfolio from "@/components/Projects/img/portfolio-old.png"
+import OldPortfolio from "@/components/Projects/img/portfolio-old.png" 
 import HTML from "@/components/Skills/svg/html.svg"
 import CSS from "@/components/Skills/svg/css.svg"
 import JS from "@/components/Skills/svg/javascript.svg"
 import BS from "@/components/Skills/svg/Bootstrap.svg"
 
-export default async function Projects() {
-    return (
+export default async function Projects(){
+    return(
         <div className="flex flex-wrap items-center justify-evenly">
-            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old} />
-            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old} />
-            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old} />
-            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old} />
+            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old}/>
+            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old}/>
+            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old}/>
+            <ThreeDCardDemo title={"My Portfolio Old"} note={"Portfolio designed to showcase my digital presence."} img={OldPortfolio} link={"https://shadowmonarch0007.github.io/WHAT-S-UP-HOMIES-old/"} people={Portfolio_Old}/>
         </div>
     );
 }
 
 const Portfolio_Old = [
     {
-        id: 4,
-        name: "Bootstrap",
-        image: BS,
+      id: 1,
+      name: "Bootstrap",
+      image: BS,
     },
     {
-        id: 1,
-        name: "HTML",
-        image: HTML,
+      id: 2,
+      name: "HTML",
+      image: HTML,
     },
     {
-        id: 2,
-        name: "CSS",
-        image: CSS,
+      id: 3,
+      name: "CSS",
+      image: CSS,
     },
     {
-        id: 3,
-        name: "Javascript",
-        image: JS,
+      id: 4,
+      name: "Javascript",
+      image: JS,
     },
-];
+  ];
