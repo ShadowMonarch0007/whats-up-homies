253b91046089f34cbd71c1ecd18aba19e791d302
Aditya Chandra <aditya.anil.chandra@gmail.com>
2024-06-01 23:51:25 +0530

    29.05.2024

--- src/components/Connect/subcomponents/form.jsx
+++ src/components/Connect/subcomponents/form.jsx
@@ -19,10 +19,10 @@ export function ConnectFormDemo() {
     <div className="max-w-4xl w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-[#e0e0e0]/50 dark:bg-[#202020]/50 border-4
     border-[#e0e0e0] dark:border-[#202020]">
       <h2 className="font-bold text-xl lg:text-2xl text-neutral-800 dark:text-neutral-200">
-        Let's get in touch
+        Lets get in touch
       </h2>
       <p className="text-neutral-600 text-sm lg:text-medium mt-2 dark:text-neutral-300">
-      Reach out to me today and let's discuss how I can help you achieve your goals.
+      Reach out to me today and lets discuss how I can help you achieve your goals.
       </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
@@ -48,10 +48,10 @@ export function ConnectFormDemo() {
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="bg-gradient-to-br relative group/btn from-zinc-100 dark:from-zinc-900 dark:to-zinc-900 to-neutral-100 block bg-zinc-50 dark:bg-zinc-800 w-full text-black dark:text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Send this to me;
+          Send this to me &rarr;
           <BottomGradient />
         </button>
       </form>
@@ -62,8 +62,8 @@ export function ConnectFormDemo() {
 const BottomGradient = () => {
   return (
     <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-900 dark:via-cyan-500 to-transparent" />
+      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-900 dark:via-indigo-500 to-transparent" />
     </>
   );
 };
