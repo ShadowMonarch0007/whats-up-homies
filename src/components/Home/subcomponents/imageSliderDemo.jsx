e5d6f454a5054b6cb4857856ec320e9ea5742022
Aditya Chandra <aditya.anil.chandra@gmail.com>
2025-01-08 15:03:22 +0530

    project stack changed

--- src/components/Home/subcomponents/imageSliderDemo.jsx
+++ src/components/Home/subcomponents/imageSliderDemo.jsx
@@ -6,12 +6,18 @@ import { ImagesSlider } from "@/components/ui/images-slider";
 import portfolio from "@/components/Home/img/portfolio.png";
 import campusXpress from "@/components/Home/img/campusXpress.png";
 import portfolioOld from "@/components/Projects/img/portfolio-old.png";
+import CloudHaven from "@/components/Projects/img/cloud-haven.png";
+import GameX from "@/components/Projects/img/game-x-home.png";
+import Jarvis from "@/components/Projects/img/jarvis-ai.png";
+import JarvisI from "@/components/Projects/img/jarvis-i.png";
 
 export function ImagesSliderDemo() {
     const images = [
+        CloudHaven,
+        Jarvis,
+        JarvisI,
+        GameX,
         portfolio,
-        campusXpress,
-        portfolioOld,
     ];
     return (
       <ImagesSlider className="h-40 w-60 lg:h-80 lg:w-[600px] rounded-xl" images={images}>
